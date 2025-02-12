e382acd78ffaa0f0d3bb199666d33a35cee4b3c8
vivek <31vivekpal@gmail.com>
2025-02-12 13:28:03 +0700

    fix: network id

--- crates/windexer-network/src/node/keypair.rs
+++
@@ -1,9 +0,0 @@
-use {
-    libp2p::identity,
-    solana_sdk::signer::keypair::Keypair as SolanaKeypair,
-};
-
-pub fn convert_keypair(solana_keypair: &SolanaKeypair) -> identity::Keypair {
-    let secret = solana_keypair.to_bytes();
-    identity::Keypair::ed25519_from_bytes(secret).expect("Valid keypair conversion")
-} 
