7bf00c572c31e4244a54bb53ffe5e22af66ce1fe
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2025-04-07 14:39:23 +0530

    feat: created terraform code for 2 azure vm creation

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -1,44 +1,63 @@
+locals {
+  vms = {
+    "us-vm" = {
+      location = "eastus"
+      name     = "${var.prefix}-us"
+    }
+    "asia-vm" = {
+      location = "southeastasia"
+      name     = "${var.prefix}-asia"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-resources"
-  location = "eastus"
+  for_each = local.vms
+  name     = "${each.value.name}-rg"
+  location = each.value.location
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
+  for_each            = local.vms
+  name                = "${each.value.name}-vnet"
   address_space       = ["10.0.0.0/22"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = each.value.location
+  resource_group_name = azurerm_resource_group.main[each.key].name
 }
 
 resource "azurerm_subnet" "internal" {
+  for_each             = local.vms
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.main[each.key].name
+  virtual_network_name = azurerm_virtual_network.main[each.key].name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  for_each            = local.vms
+  name                = "${each.value.name}-nic"
+  resource_group_name = azurerm_resource_group.main[each.key].name
+  location            = each.value.location
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = azurerm_subnet.internal[each.key].id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = "eastus"
+  for_each                        = local.vms
+  name                            = "${each.value.name}-vm"
+  resource_group_name             = azurerm_resource_group.main[each.key].name
+  location                        = each.value.location
   size                            = "Standard_B2s"
   admin_username                  = "adminuser"
   admin_password                  = "@Windexer"
   disable_password_authentication = false
+
   network_interface_ids = [
-    azurerm_network_interface.main.id,
+    azurerm_network_interface.main[each.key].id,
   ]
 
   source_image_reference {
--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -2,10 +2,6 @@ variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
 }
 
-variable "location" {
-  description = "eastus"
-}
-
 variable "client_id" {}
 variable "client_secret" {}
 variable "subscription_id" {}
